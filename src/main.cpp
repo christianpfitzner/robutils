6f9fea7a1bad613f921c13f23be71beeb03f90d2
Prof. Dr. Christian Pfitzner <christian.pfitzner@thi.de>
2021-02-07 23:21:00 +0100

    -- initial commit for utility library

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "types.h"
+
+int main() {
+    std::cout << "Bla: " << std::endl;
+    return 0;
+}
---
+++ src/types.h
@@ -0,0 +1,33 @@
+/* Copyright (C) 2020 Prof. Dr. Christian Pfitzner - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the BSD license, which unfortunately won't be
+ * written for another century.
+ */
+
+
+
+
+#if !defined(ROBOUTILS_TYPES)
+#define ROBOUTILS_TYPES
+
+#include <Eigen/Dense>
+#include <vector>
+
+
+using Point2D      = Eigen::Vector2d; 
+using Point3D      = Eigen::Vector3d; 
+
+using PointCloud2D = std::vector<Point2D>; 
+using PointCloud3D = std::vector<Point3D>; 
+
+using RotMat2D     = Eigen::Matrix2d; 
+using RotMat3D     = Eigen::Matrix3d; 
+
+
+
+#endif // ROBOUTILS_TYPES
+
+
+
+
+
